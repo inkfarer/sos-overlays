1bfe5f285dd95647bade9c2f760f7ca159504530
inkfarer <inkfarer@inkfarer.com>
2020-09-16 15:46:29 +0300

    check for undefined strings when adding dots

--- dashboard/tourneyImport.js
+++ dashboard/tourneyImport.js
@@ -66,6 +66,7 @@ submitId.onclick = () => {
 }
 
 function addDots(string) {
+	if (!string) return string;
 	if (string.length >= maxNameLength) return string.substring(0, maxNameLength) + '...';
 	else return string;
 }
