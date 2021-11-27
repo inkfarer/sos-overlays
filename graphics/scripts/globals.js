59dc51dbbf3e1fad5c7e77c1ba899830d29b3ac1
inkfarer <inkfarer@inkfarer.com>
2021-11-27 19:31:57 +0200

    Refactoring; Port main graphic to 3.0.0

--- graphics/scripts/globals.js
+++ graphics/scripts/globals.js
@@ -1,1 +1,52 @@
 const DASHBOARD_BUNDLE_NAME = 'ipl-overlay-controls';
+
+function doOnDifference(newValue, oldValue, path, callback) {
+    const newObject = _.get(newValue, path);
+    const oldObject = _.get(oldValue, path);
+
+    if (newObject != null && (oldObject == null || !_.isEqual(newObject, oldObject))) {
+        callback(newObject, oldObject);
+    }
+}
+
+function doOnOneOrMoreDifference(newValue, oldValue, paths, callback) {
+    const newPaths = _.at(newValue, paths);
+    const oldPaths = _.at(oldValue, paths);
+
+    const doesNotExist = value => value == null;
+
+    if (!newPaths.every(doesNotExist) && (oldPaths.every(doesNotExist) || !_.isEqual(newPaths, oldPaths))) {
+        callback(newPaths);
+    }
+}
+
+function doOnNoDifference(newValue, oldValue, path, callback) {
+    const newObject = _.get(newValue, path);
+    const oldObject = _.get(oldValue, path);
+
+    if (newObject != null && (oldObject == null || _.isEqual(newObject, oldObject))) {
+        callback(newObject);
+    }
+}
+
+function loadImagePromise(imageUrl) {
+    return new Promise((resolve) => {
+        const imageLoaderElem = document.createElement("img");
+        imageLoaderElem.src = imageUrl;
+
+        imageLoaderElem.addEventListener('load', () => {
+            resolve();
+        });
+    })
+}
+
+function addDots(value, maxLength = 48) {
+    const rolloff = '...';
+
+    if (!value) return value;
+    if (value.length > maxLength) {
+        return value.substring(0, maxLength - rolloff.length) + rolloff;
+    }
+
+    return value;
+}
