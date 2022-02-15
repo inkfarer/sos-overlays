528f9cefff2f02a689b9b383e503286c37018da4
inkfarer <inkfarer@inkfarer.com>
2022-02-15 05:08:40 +0200

    Fix scoreboard not hiding as expected when toggling visibility multiple times in quick succession

--- graphics/scripts/main/scoreboard.js
+++ graphics/scripts/main/scoreboard.js
@@ -15,29 +15,37 @@ activeRound.on('change', (newValue) => {
 	});
 });
 
+const scoreboardShowTl = gsap.timeline();
+
 scoreboardData.on('change', (newValue, oldValue) => {
 	document.querySelector('.sbFlavorTextBG fitted-text').setAttribute('text', newValue.flavorText);
 
 	doOnDifference(newValue, oldValue, 'isVisible', isVisible => {
-		let lineClipPath, lineDelay, bgTop, bgEase, bgDelay, flavorOpacity;
+		let lineClipPath, lineDelay, bgTop, bgEase, bgDelay, flavorOpacity, flavorDelay;
 		if (isVisible) {
 			lineClipPath = 'polygon(0px 0px, 300px 0px, 300px 300px, 0px 300px)';
 			bgTop = '0px';
 			bgEase = 'power2.out';
-			bgDelay = 0.4;
+			bgDelay = 0.6;
 			lineDelay = 0;
+			flavorDelay = 0.75;
 			flavorOpacity = 1;
 		} else {
 			lineClipPath = 'polygon(0px 0px, 0px 0px, 0px 300px, 0px 300px)';
 			bgTop = '90px';
 			bgEase = 'power2.in';
 			bgDelay = 0;
-			lineDelay = 0.4;
+			lineDelay = 0.6;
+			flavorDelay = 0.6;
 			flavorOpacity = 0;
 		}
 
-		gsap.to('.sbBrandLine', {duration: 0.5, clipPath: lineClipPath, ease: 'power2.inOut', delay: lineDelay});
-		gsap.to('.sbBackground', {duration: 0.5, top: bgTop, ease: bgEase, delay: bgDelay});
-		gsap.to('.sbFlavorTextBG', {duration: 0.5, opacity: flavorOpacity, ease: 'power2.inOut', delay: lineDelay});
+		const sbTl = gsap.timeline();
+
+		sbTl.to('.sbBrandLine', {duration: 0.5, clipPath: lineClipPath, ease: 'power2.inOut', delay: lineDelay}, 'show-hide')
+		.to('.sbBackground', {duration: 0.5, top: bgTop, ease: bgEase, delay: bgDelay}, 'show-hide')
+		.to('.sbFlavorTextBG', {duration: 0.5, opacity: flavorOpacity, ease: 'power2.inOut', delay: flavorDelay}, 'show-hide');
+
+		scoreboardShowTl.add(sbTl);
 	});
 });
